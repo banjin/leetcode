587a7df4445b20fe94ab27d835cf37b98db7c3e6
songhm <baliang616@163.com>
2019-11-27 20:20:42 +0800

    去除有序数组中重复数字

---
+++ remove-duplicates-from-sorted-array.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+class Solutions:
+    def remove_duplication(self, nums):
+        if len(nums) <= 1:
+            return len(nums)
+        
+        k = 1 #用于记录前一元素
+        while(k < len(nums)):
+            if nums[k] == nums[k-1]:
+                del nums[k]
+            else:
+                k += 1
+        return k
+
+
+def main():
+    s = Solutions()
+    result = s.remove_duplication([1,1,2])
+    print(result)
+
+if __name__ == "__main__":
+    print('ddd')
+    main()
